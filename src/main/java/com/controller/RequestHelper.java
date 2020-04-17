fbac1921ab4b50ab006dfce93695701be70f3c55
m-szabel <61705224+m-szabel@users.noreply.github.com>
2020-04-17 14:09:26 -0400

    new commit

---
+++ src/main/java/com/controller/RequestHelper.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.controller.ClownController;
+
+public class RequestHelper {
+
+    public static String process(HttpServletRequest req) {
+        System.out.println(req.getRequestURI());
+        switch(req.getRequestURI()) {
+        case "/FrontControllerProject/landing.master":
+            return LandingController.goHome();
+        case "/FrontControllerProject/gonext.master":
+            return "html/second.html";
+        default:
+            return "";
+        }
+    }
+    
+    public static void directProcess(HttpServletRequest req, HttpServletResponse res) {
+        switch(req.getRequestURI()) {
+        case "/FrontControllerProject/clown.json":
+            new ClownController().getById(req, res);
+        }
+    }
+}
---
+++ src/main/java/com/example/model/Clown.java
@@ -0,0 +1,58 @@
+package com.example.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.sun.xml.internal.txw2.annotation.XmlElement;
+
+@XmlRootElement(name="Clown")
+public class Clown {
+
+    private int id;
+    private String name;
+    private boolean scary;
+
+    @XmlElement
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @XmlElement
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlElement
+    public boolean isScary() {
+        return scary;
+    }
+
+    public void setScary(boolean scary) {
+        this.scary = scary;
+    }
+
+    public Clown(int id, String name, boolean scary) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.scary = scary;
+    }
+
+    public Clown() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String toString() {
+        return "Clown [id=" + id + ", name=" + name + ", scary=" + scary + "]";
+    }
+
+}
